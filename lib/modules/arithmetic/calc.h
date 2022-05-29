04c95e6986ef4b5417463744e8d7f32b5589cfa2
Choleski Louis <choleski@gmx.com>
2022-05-29 13:01:09 -0400

    Refactor arithmetic calc.

--- lib/modules/arithmetic/calc.h
+++ lib/modules/arithmetic/calc.h
@@ -34,36 +34,26 @@ int set_precision(int p) {
 
 napi_value calc(napi_env env, napi_callback_info info){
 
+	char* pend;
+
 	int nbr_args = 4;
+
+	napi_value response;
 
 	size_t argc = nbr_args;
 	napi_value argv[nbr_args];
-	
-	napi_value response;
 
-	int p = 0;
-	int precision = 8;
+	int p = 0, precision = 8;
 
-	char* pend;
+	char a[10], b[10], o[10] = "";
 
 	char buf[MAX_BUFFER_SIZE] = "";
 
-	char a[10] = "";
-	char b[10] = "";
-	char o[10] = "";
+	size_t a_copied, b_copied, o_copied;
 
-	size_t a_copied;
-	size_t b_copied;
-	size_t o_copied;
+	double fa = 0.00, fb = 0.00, amount = 0.00;
 
-	double fa = 0.00;
-	double fb = 0.00;
-
-	double amount = 0.00;
-
-	size_t a_size = sizeof(a);
-	size_t b_size = sizeof(b);
-	size_t o_size = sizeof(o);
+	size_t a_size = sizeof(a), b_size = sizeof(b), o_size = sizeof(o);
 
 	// Parse arguments.
 	napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
@@ -74,13 +64,12 @@ napi_value calc(napi_env env, napi_callback_info info){
 	napi_get_value_string_utf8(env, argv[2], &b, b_size, &b_copied);
 	napi_get_value_string_utf8(env, argv[1], &o, o_size, &o_copied);
 
-	// Map required input fields.
+	// Convert string params to double.
 	fa = strtof(a, &pend);
 	fb = strtof(b, NULL);
 
+	// Set precision size.
 	precision = set_precision(p);
-
-	/* ------------ [ Arithmetical utilities ] ------------ */
 
 	if (!strcmp(o, "plus")) {
 		amount = plus(fa, fb);
