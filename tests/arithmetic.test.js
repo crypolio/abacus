4d5817383a466594a15d1cffb3f5f6571db7ab8b
Choleski Louis <choleski@gmx.com>
2022-12-29 06:39:47 -0500

    Fix tests precision.

--- tests/arithmetic.test.js
+++ tests/arithmetic.test.js
@@ -4,32 +4,32 @@ describe('arithmetical tests', () => {
 
 	it('addition', () => {
 		const res = Abacus('1').plus('2').toString();
-		expect(res).toEqual('3.000000');
+		expect(res).toEqual('3.00000000');
 	});
 
 	it('substraction', () => {
 		const res = Abacus('5').min('2').toString();
-		expect(res).toEqual('3.000000');
+		expect(res).toEqual('3.00000000');
 	});
 
 	it('multiplication', () => {
 		const res = Abacus('5').mul('2').toString();
-		expect(res).toEqual('10.000000');
+		expect(res).toEqual('10.00000000');
 	});
 
 	it('division', () => {
 		const res = Abacus('15').div('3').toString();
-		expect(res).toEqual('5.000000');
+		expect(res).toEqual('5.00000000');
 	});
 
 	it('modlo', () => {
 		const res = Abacus('5').mod('15').toString();
-		expect(res).toEqual('5.000000');
+		expect(res).toEqual('5.00000000');
 	});
 
 	it('chaning', () => {
 		const res = Abacus('1').plus('2').mul('5').div('1').mod('9').toString();
-		expect(res).toEqual('6.000000');
+		expect(res).toEqual('6.00000000');
 	});
 
 });
